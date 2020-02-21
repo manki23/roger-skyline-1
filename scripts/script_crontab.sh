406b62cbf3976a20f0d31a34008d3f7396a32ecc
Myriam ANKI <manki@student.42.fr>
2020-02-21 19:24:13 +0100

    updating script

--- scripts/script_crontab.sh
+++ scripts/script_crontab.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
-
-cat /etc/crontab > /root/scripts/new
-DIFF=$(diff /root/scripts/new /root/scripts/tmp)
-if [ "$DIFF" != "" ]; then
-	cp mail_type.txt mail.txt
+#!/bin/bash
+if [[ $(find /etc -name "crontab" -mtime 0) ]]; then
+	cp /root/scripts/mail_type.txt mail.txt
 	diff /root/scripts/new /root/scripts/tmp >> mail.txt
 	cat mail.txt | sudo sendmail -v manki@student.42.fr
 	rm -f /root/scripts/tmp
 	cp /root/scripts/new /root/scripts/tmp
 fi
+
