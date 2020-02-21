cd1b55a686793496b30fe33a9bc1f8af72db9db9
Myriam ANKI <manki@student.42.fr>
2020-02-21 20:07:24 +0100

    script_crontab.sh finnally fonctionnal

--- scripts/script_crontab.sh
+++ scripts/script_crontab.sh
@@ -6,5 +6,6 @@ if [[ $(find /etc -name "crontab" -mtime 0) ]]; then
 	cat mail.txt | sudo sendmail -v manki@student.42.fr
 	rm -f /root/scripts/tmp
 	mv /root/scripts/new /root/scripts/tmp
+	rm mail.txt
 fi
 
